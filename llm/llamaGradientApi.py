9578a36dc73f18b95e4d17ebb4ebf9ec9fbf8a89
master-ladvil <srihariharanb21@gmail.com>
2025-07-16 00:16:59 +0530

    [deleted everything ]

--- llm/llamaGradientApi.py
+++
@@ -1,9 +0,0 @@
-from callLlama import CallLlama
-
-def main():
-    llama = CallLlama('llama3-gradient')
-    response = llama.askLlama("hi celestial")
-    print(response)
-
-if __name__ == "__main__":
-    main()
